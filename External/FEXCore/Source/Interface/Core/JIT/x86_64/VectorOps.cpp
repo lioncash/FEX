c75a0b9d86a4b3f84b573564a45ba4d6b4c880a4
lioncash <mathew1800@gmail.com>
2022-08-11 14:13:20 -0400

    VectorOps: Handle 256-bit VUMax

--- External/FEXCore/Source/Interface/Core/JIT/x86_64/VectorOps.cpp
+++ External/FEXCore/Source/Interface/Core/JIT/x86_64/VectorOps.cpp
@@ -1075,20 +1075,27 @@ DEF_OP(VSMin) {
 
 DEF_OP(VUMax) {
   auto Op = IROp->C<IR::IROp_VUMax>();
+
+  const auto Dst = ToYMM(GetDst(Node));
+  const auto Vector1 = ToYMM(GetSrc(Op->Vector1.ID()));
+  const auto Vector2 = ToYMM(GetSrc(Op->Vector2.ID()));
+
   switch (Op->Header.ElementSize) {
     case 1: {
-      vpmaxub(GetDst(Node), GetSrc(Op->Vector1.ID()), GetSrc(Op->Vector2.ID()));
+      vpmaxub(Dst, Vector1, Vector2);
       break;
     }
     case 2: {
-      vpmaxuw(GetDst(Node), GetSrc(Op->Vector1.ID()), GetSrc(Op->Vector2.ID()));
+      vpmaxuw(Dst, Vector1, Vector2);
       break;
     }
     case 4: {
-      vpmaxud(GetDst(Node), GetSrc(Op->Vector1.ID()), GetSrc(Op->Vector2.ID()));
+      vpmaxud(Dst, Vector1, Vector2);
       break;
     }
-    default: LOGMAN_MSG_A_FMT("Unknown Element Size: {}", Op->Header.ElementSize); break;
+    default:
+      LOGMAN_MSG_A_FMT("Unknown Element Size: {}", Op->Header.ElementSize);
+      break;
   }
 }
 
